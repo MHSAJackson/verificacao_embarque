deac24424bb6bf810a71feca851cd94f29ca90c6
jackson <jackson_wie@hotmail.com>
2020-06-22 10:18:50 -0300

    Teste

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <title>Teste Git</title>
+    <meta charset="ISO 8859-1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+</head>
+
+<body>
+    <table style="width:100%">
+        <tr>
+            <th>Jackson</th>
+            <th>Lastname</th>
+            <th>Age</th>
+        </tr>
+        <tr>
+            <td>Teste</td>
+            <td>Smith</td>
+            <td>50</td>
+        </tr>
+        <tr>
+            <td>Eve</td>
+            <td>Jackson</td>
+            <td>94</td>
+        </tr>
+    </table>
+
+</body>
+
+</html>
